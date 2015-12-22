138ab7060c89b226a08190dc27b74ed0fa66f8d8
Person McPivot <person.mcpivot@pivotal.io>
2015-12-22 14:39:47 +0900

    Adds restaurants#create route

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,21 @@ class RestaurantsController < ApplicationController
   def index
     render json: Restaurant.all
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      render json: @restaurant
+    else
+      render json: @restaurant.errors, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name)
+    end
 end
--- spec/requests/restaurants_api_spec.rb
+++ spec/requests/restaurants_api_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
 
 describe 'Restaurants API' do
+  def json_response
+    JSON.parse(response.body)
+  end
+
   describe 'get restaurants' do
-    def json_response
-      JSON.parse(response.body)
-    end
 
     let!(:tsukemen) { Restaurant.create!(name: "Tukemen TETSU") }
     let!(:curry)    { Restaurant.create!(name: "Coco Curry") }
 
     it 'returns all restaurants' do
       get '/restaurants', format: :json
-
       expect(json_response.count).to eq 2
       expect(json_response[0]['name']).to eq "Tukemen TETSU"
       expect(json_response[1]['name']).to eq "Coco Curry"
     end
+  end
+
+  describe 'create restaurant' do
+    let(:restaurant_json) { { restaurant: { name: 'Tonkatsu' } } }
+    it 'creates the restaurant' do
+      expect {
+        post '/restaurants', restaurant_json, format: :json
+      }.to change { Restaurant.count }.by(1)
+    end
+
+    it 'returns created restaurant' do
+      post '/restaurants', restaurant_json, format: :json
+      puts json_response
+      expect(json_response['name']).to eq "Tonkatsu"
+    end
+
+
   end
 end
