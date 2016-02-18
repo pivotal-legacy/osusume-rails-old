f3af17effd139ed4ee51e30a5f9754d7f581f61c
Yuki Nishijima <ynishijima@pivotal.io>
2016-02-18 14:49:26 +0900

    Stop defining methods directly on the main object

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
-def json_response
-  JSON.parse(response.body)
+module RequestSpecHelper
+  def json_response
+    JSON.parse(response.body)
+  end
 end
 
 RSpec.configure do |config|
+  config.include RequestSpecHelper, type: :request
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
